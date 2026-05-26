ef6f672ffc3adafc11608886597ea568993b132a
Henrique <henrique.fabro@escola.pr.gov.br>
2026-05-26 18:12:46 -0300

    aula02

--- aula01.js
+++ aula01.js
@@ -1,8 +1,10 @@
+//exercício 01
 const nome = "Bianca";
 const idade = 16;
 const cidade = "Cascavel";
 console.log(`Meu nome é ${nome}, tenho ${idade} anos e moro em ${cidade}.`);
 
+//exercício 02
 const somar = (a, b) => a + b;
 const subtrair = (a, b) => a - b;
 const multiplicar = (a, b) => a * b;
@@ -12,6 +14,7 @@ console.log(`Subtração: ${subtrair(10, 5)}`);
 console.log(`Multiplicação: ${multiplicar(10, 5)}`);
 console.log(`Divisão: ${dividir(10, 5)}`);
 
+//exercício 03
 const input = require('readline-sync');
 function classificar(nota) {
   if (nota >= 7) {
@@ -25,13 +28,15 @@ function classificar(nota) {
 let nota = Number(input.question("Digite nota do aluno: "));
 console.log("Resultado:", classificar(nota));
 
-function tabuada = (numero) => {
+//exercício 04
+function tabuada (numero) {
     for (let i = 1; i <= 10; i++) {
         console.log(`${numero} x ${i} = ${numero * i}`);
     }
 };
 tabuada(5);
 
+//exercício 05
 let valor;
 if (valor) {
     console.log("Tem valor");
---
+++ aula02.js
@@ -0,0 +1,58 @@
+//exercício 01
+// const nome = "Bianca";
+// const idade = 16;
+// const cidade = "Cascavel";
+// console.log(`Meu nome é ${nome}, tenho ${idade} anos e moro em ${cidade}.`);
+
+//exercício 02
+// const prompt = require("prompt-sync")();
+
+// let n1 = Number(prompt("Digite um número: "))
+// let n2 = Number(prompt("Digite um número: "))
+
+// console.log(`Soma: ${(n1 + n2)}`)
+// console.log(`Subtração: ${(n1 - n2)}`)
+// console.log(`Multiplicação: ${(n1 * n2)}`)
+// if (n2 != 0){
+//     console.log(`Divisão: ${(n1 / n2)}`)
+// }else{
+//     console.log(`Número inválido`)
+// }
+
+//exercício 03
+const prompt = require("prompt-sync")();
+
+// let soma = 0
+// let n1
+// for (let i = 0; i < 5; i++){
+//     let n1 = Number(prompt("Digite a nota: "))
+//     soma = soma + n1
+// }
+// let media = soma / 5
+// if(media >= 7){
+//     console.log("Aluno aprovado!")
+// }else if(media >= 5){
+//     console.log("Aluno em recuperação!")
+// }else{
+//     console.log("Aluno reprovado!")
+// }
+
+//exercício 04
+// let n1 = Number(prompt("Digite um número: "))
+// for (let i = 1; i <= 10; i++) {
+//     console.log(`${n1} x ${i} = ${n1 * i}`);
+// }
+
+//exercício 05
+
+
+
+
+
+
+
+
+
+
+
+
